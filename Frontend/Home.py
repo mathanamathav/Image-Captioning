3963af31b854efb6495bcee47f295c6efeaf6a20
Mathana Mathav <asmathav01@gmail.com>
2023-03-31 12:21:41 +0530

    improve app

--- Frontend/Home.py
+++ Frontend/Home.py
@@ -1,23 +1,30 @@
 import streamlit as st
 from PIL import Image
+import matplotlib.pyplot as plt
+from pickle import load
+from keras.models import load_model
+import utils
+
+xception_model = utils.cnn_model()
 
 # Set up the Streamlit app
 st.title("Image Captioning")
 
 # Add a file upload widget and get the uploaded file
-uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader(
+    "Choose an image file", type=["jpg", "jpeg", "png"])
 
 # If a file was uploaded
 if uploaded_file is not None:
     # Load the image from the uploaded file
     image = Image.open(uploaded_file)
-    
-    # Display the image
+
+    tokenizer = load(open("./models/tokenizer.p", "rb"))
+    model = load_model('./models/model_7.h5')
+    max_length = 32
+
+    photo = utils.extract_features(image, xception_model)
+    description = utils.predict_caption(model, tokenizer, photo, max_length)
+
+    st.subheader("Text extracted from the image: {}".format(description))
     st.image(image, caption='Uploaded Image', use_column_width=True)
-    
-    
-    # Display the text in a text box
-    st.write("Text extracted from the image:")
-    # st.text('\n'.join([res[1] for res in result]))
-
-
---
+++ Frontend/utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from keras.applications.xception import Xception
+from keras.utils import pad_sequences
+import argparse
+
+def cnn_model():
+  xception_model = Xception(include_top=False, pooling="avg")
+  return xception_model
+
+def extract_features(image, model):
+  image = image.resize((299,299))
+  image = np.array(image)
+  # for images that has 4 channels, we convert them into 3 channels
+  if image.shape[2] == 4: 
+      image = image[..., :3]
+  image = np.expand_dims(image, axis=0)
+  image = image/127.5
+  image = image - 1.0
+  feature = model.predict(image)
+  return feature
+
+def word_for_id(integer, tokenizer):
+  for word, index in tokenizer.word_index.items():
+    if index == integer:
+      return word
+  return None
+
+
+def predict_caption(model,tokenizer,image,maxlen=32):
+  '''
+  image.shape = (1,4462)
+  '''
+  index_word = dict([(index,word) for word, index in tokenizer.word_index.items()])
+  in_text = 'start'
+
+  for iword in range(maxlen):
+      sequence = tokenizer.texts_to_sequences([in_text])[0]
+      sequence = pad_sequences([sequence],maxlen)
+      yhat = model.predict([image,sequence],verbose=0)
+      yhat = np.argmax(yhat)
+      newword = index_word[yhat]
+      in_text += " " + newword
+      if newword == "end":
+          break
+  return(in_text)
